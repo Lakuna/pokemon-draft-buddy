faae81c2c2f3a29bc88f910e1a18b3eca947e86a
Travis Martin <tjmartin2003@gmail.com>
2025-10-06 03:43:15 -0500

    Generate usable JSON with relevant data.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
+import IsNonstandard from "./types/IsNonstandard.js";
 import fetchAnalysis from "./utility/fetchPokemon.js";
 import fetchBasics from "./utility/fetchBasics.js";
 import fetchGenerations from "./utility/fetchGenerations.js";
 import htmlToPlaintext from "./utility/htmlToPlaintext.js";
+import { writeFile } from "node:fs/promises";
 
 const generations = await fetchGenerations();
 if (!generations) {
 	throw new Error("Failed to fetch generations.");
 }
 
-const generation = generations.find(({ shorthand }) => shorthand === "SV");
+const generation = generations[generations.length - 1];
 if (!generation) {
 	throw new Error("Failed to choose a generation.");
 }
@@ -21,53 +23,92 @@ if (!basics) {
 	throw new Error("Failed to fetch basics.");
 }
 
-const pokemon = basics.pokemon.find(({ name }) => name === "Gholdengo");
-if (!pokemon) {
-	throw new Error("Failed to choose a Pokémon.");
-}
-
 // eslint-disable-next-line no-console
-console.log(`Using Pokémon: ${pokemon.name}.`);
+console.log(`Got basics with ${basics.pokemon.length.toString()} Pokémon.`);
 
-const analysis = await fetchAnalysis(generation, pokemon);
-if (!analysis) {
-	throw new Error("Failed to fetch an analysis.");
+const all = basics.pokemon.filter(
+	({ isNonstandard }) => isNonstandard !== IsNonstandard.CAP
+);
+const out = [];
+for (const [i, pokemon] of all.entries()) {
+	// eslint-disable-next-line no-console
+	console.log(
+		`Getting Pokémon: ${pokemon.name} (${i.toString()}/${all.length.toString()} | ${((i / all.length) * 100).toFixed()}%).`
+	);
+
+	// eslint-disable-next-line no-await-in-loop
+	const analysis = await fetchAnalysis(generation, pokemon);
+	if (!analysis?.strategies) {
+		continue;
+	}
+
+	const strategy = analysis.strategies.find(({ format }) => format === "Draft");
+	if (!strategy) {
+		continue;
+	}
+
+	// Parse the overview.
+	// eslint-disable-next-line no-await-in-loop
+	const overview = await htmlToPlaintext(strategy.overview);
+	const [draftOrderTextLabeled, priceRangeTextLabeled] = overview.split("\n");
+	const draftOrderText = draftOrderTextLabeled
+		?.substring(draftOrderTextLabeled.indexOf(":") + 1)
+		.trim();
+	const draftOrder = parseInt(draftOrderText?.match(/\d+/gu)?.[0] ?? "", 10);
+	const priceRangeText = priceRangeTextLabeled
+		?.substring(priceRangeTextLabeled.indexOf(":") + 1)
+		.trim();
+	const priceRange = priceRangeText?.match(/\d+/gu);
+	const priceRangeMin = parseInt(priceRange?.[0] ?? "", 10);
+	const priceRangeMax = parseInt(priceRange?.[1] ?? "", 10);
+
+	// Parse the comments.
+	// eslint-disable-next-line no-await-in-loop
+	const comments = await htmlToPlaintext(strategy.comments);
+	const commonRoles: Record<string, string> = {};
+	let inCommonRoles = false;
+	for (const line of comments.split("\n")) {
+		const cleanLine = line.toLowerCase().trim();
+		if (cleanLine === "common roles") {
+			inCommonRoles = true;
+			continue;
+		}
+
+		if (
+			cleanLine === "common moves" ||
+			cleanLine === "niche moves" ||
+			cleanLine === "common items" ||
+			cleanLine === "niche items" ||
+			cleanLine === "tera" ||
+			cleanLine === "draft strategy" ||
+			cleanLine === "checks and counters"
+		) {
+			inCommonRoles = false;
+			continue;
+		}
+
+		if (!inCommonRoles) {
+			continue;
+		}
+
+		commonRoles[line.substring(0, line.indexOf(":"))] = line
+			.substring(line.indexOf(":") + 1)
+			.trim();
+	}
+
+	out.push({
+		commonRoles,
+		draftOrder: {
+			round: draftOrder,
+			text: draftOrderText
+		},
+		name: pokemon.name,
+		priceRange: {
+			max: priceRangeMax,
+			min: priceRangeMin,
+			text: priceRangeText
+		}
+	});
 }
 
-if (!analysis.strategies) {
-	throw new Error("Analysis has no strategies.");
-}
-
-const strategy = analysis.strategies.find(({ format }) => format === "Draft");
-if (!strategy) {
-	throw new Error("Failed to choose a strategy.");
-}
-
-const comments = await htmlToPlaintext(strategy.comments);
-const overviewRaw = await htmlToPlaintext(strategy.overview);
-const [draftOrderTextLabeled, priceRangeTextLabeled, overviewLabeled] =
-	overviewRaw.split("\n");
-const draftOrderText = draftOrderTextLabeled
-	?.substring(draftOrderTextLabeled.indexOf(":") + 1)
-	.trim();
-const draftOrder = parseInt(draftOrderText?.match(/\d+/gu)?.[0] ?? "", 10);
-const priceRangeText = priceRangeTextLabeled
-	?.substring(priceRangeTextLabeled.indexOf(":") + 1)
-	.trim();
-const priceRange = priceRangeText?.match(/\d+/gu);
-const priceRangeMin = parseInt(priceRange?.[0] ?? "", 10);
-const priceRangeMax = parseInt(priceRange?.[1] ?? "", 10);
-const overview = overviewLabeled
-	?.substring(overviewLabeled.indexOf(":") + 1)
-	.trim();
-
-// eslint-disable-next-line no-console
-console.log({
-	comments,
-	draftOrder,
-	draftOrderText,
-	overview,
-	priceRangeMax,
-	priceRangeMin,
-	priceRangeText
-});
+await writeFile("out.json", JSON.stringify(out));
